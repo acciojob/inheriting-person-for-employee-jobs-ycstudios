b048129b79bb128bb1bcee7eea87ca2b4b11ef34
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-10 17:28:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-// complete this js code
-function Person(name, age) {}
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-function Employee(name, age, jobTitle) {}
+    greet() {
+        return `Hello, my name is ${this.name}, I am ${this.age} years old.`;
+    }
+}
+
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age); // Calling the parent class constructor first
+        this.jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
+    }
+}
 
 // Do not change code below this line
 window.Person = Person;
